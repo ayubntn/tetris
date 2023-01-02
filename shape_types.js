fcb280063ea08aad3c02d7159e5299ea1325aada
ayubntn <coccoboy_am@yahoo.co.jp>
2023-01-02 20:34:04 +0900

    edit

---
+++ shape_types.js
@@ -0,0 +1,13 @@
+const SHAPE_TYPES = [
+    {
+        name: 'type0',
+        color: 0x4285f4,
+        blocks: [
+            {x: 0, y: 0},
+            {x: 1, y: 0},
+            {x: 1, y: 1},
+        ],
+        cols: 2,
+        rows: 2
+    }
+];
